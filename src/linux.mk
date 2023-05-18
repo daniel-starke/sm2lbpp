e1e3639b1b358755bbcadb955074eb24e19f236f
Daniel Starke <daniel-email@gmx.net>
2023-05-18 22:03:28 +0200

    Fixed AppVeyor/Git Actions errors.
    Signed-off-by: Daniel Starke <daniel-email@gmx.net>

--- src/linux.mk
+++ src/linux.mk
@@ -2,6 +2,6 @@ CWFLAGS = -Wall -Wextra -Wformat -pedantic -Wshadow -Wno-format -std=c99
 CFLAGS = -O2 -DNDEBUG -D_BSD_SOURCE -D_POSIX_C_SOURCE=200112L -D_XOPEN_SOURCE -mstackrealign -fno-ident -D_LARGEFILE64_SOURCE -D_FILE_OFFSET_BITS=64
 LDFLAGS = -s -fno-ident
 PATHS = 
-LIBS = -lpng -lz
+LIBS = -lpng -lz -lm
 OBJEXT = .o
 BINEXT = 
